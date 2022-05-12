a45c6e2c37a3d55eb294fee1a50e95d4fa218107
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-05-12 11:20:19 -0500

    adding Classroom-Student association

--- classroom_class.rb
+++ classroom_class.rb
@@ -1,6 +1,13 @@
 Class Classroom
 attr_accessor :label
+attr_reader :student
 
 def initialize
   @label = label
+  @students = []
+end
+
+def add_student
+    @students.push(student)
+    student.classroom = self
 end
--- student_class.rb
+++ student_class.rb
@@ -1,9 +1,16 @@
 require './person_class'
 
 Class Student < Person
+attr_accessor :classroom
+
 def initialize(age, classroom, name = 'Unknown', parent_permission: true)
   super(age, name, parent_permission)
   @classroom = classroom
+end
+
+def classrooms(classroom)
+  @classroom = classroom
+  classroom.student.push(self) unless classroom.student.include?(self)
 end
 
 def play_hooky
