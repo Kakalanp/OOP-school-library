2717b0bbf0c2a81d4c7c28c0c7ee32274e9a014c
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-05-14 12:37:00 -0500

    moving one parameter, fixing offenses

--- student_class.rb
+++ student_class.rb
@@ -3,7 +3,7 @@ require './person_class'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission)
+  def initialize(age, classroom, parent_permission, name = 'Unknown')
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
