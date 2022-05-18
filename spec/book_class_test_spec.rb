c0bf58fb71bf7f3b49ef9c2e0b3a959ffce8e81b
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-05-18 17:46:09 -0500

    adding Classroom test

--- spec/book_class_test_spec.rb
+++ spec/book_class_test_spec.rb
@@ -5,5 +5,6 @@ describe 'Book' do
     book = Book.new('12 Rules for life', 'Jordan B. Peterson')
 
     expect(book.title).to eq('12 Rules for life')
+    expect(book.author).to eq('Jordan B. Peterson')
   end
 end
