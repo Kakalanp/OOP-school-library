ab798b52cd2e1d0bf50a967ad32898086860e7f9
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-05-19 11:04:03 -0500

    Adding a book test

--- spec/book_class_spec.rb
+++ spec/book_class_spec.rb
@@ -1,10 +1,16 @@
 require './book_class'
 
 describe 'Book' do
-  it 'creates a book with the correct parameters' do
-    book = Book.new('12 Rules for life', 'Jordan B. Peterson')
+  before(:each) do
+    @book = Book.new('12 Rules for life', 'Jordan B. Peterson')
+  end
 
-    expect(book.title).to eq('12 Rules for life')
-    expect(book.author).to eq('Jordan B. Peterson')
+  it 'creates a book with the correct parameters' do
+    expect(@book.title).to eq('12 Rules for life')
+    expect(@book.author).to eq('Jordan B. Peterson')
+  end
+
+  it 'Is an instance of Rental' do
+    @book.should be_an_instance_of Book
   end
 end
